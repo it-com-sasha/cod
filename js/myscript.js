c62bb70f8a6cbd6ee8e88f2e0e2bdf38e50ca943
it-com-sasha <in-mg@ukr.net>
2020-05-02 20:04:06 +0300

    Продолжаем обучение

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,8 @@
+Vue.component('todo-item', {
+	template: '<p>Это задача из списка</p>'
+});
+
+
 var app = new Vue ({
 	el: "#app",
 	data: {
@@ -12,3 +17,46 @@ var app2 = new Vue ({
 		message: "Вы загрузили эту страницу:" + new Date().toLocaleString()
 	}
 });
+
+var app3 = new Vue ({
+	el: "#app-3",
+	data: {
+		seen: true
+	}
+});
+
+var app4 = new Vue ({
+	el: "#app-4",
+	data: {
+		todos: [
+			{ text: "Выучить JavaScript"},
+			{ text: "Выучить Vue"},
+			{ text: "Написать проект"}
+		]
+	}
+	
+});
+
+var app5 = new Vue ({
+	el: "#app-5",
+	data: {
+		message: "Привет это Vue"
+	},
+	methods: {
+		reverseMessage : function() {
+			this.message = this.message.split('').reverse().join('')
+		}
+	} 
+	
+});
+
+var app6 = new Vue ({
+	el: "#app-6",
+	data: {
+		message: "Введите текст"
+	}
+	
+});
+
+
+
